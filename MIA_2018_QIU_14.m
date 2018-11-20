0a77e788ae282900d6c50c328647cacb838dc8ee
野比chiyann <45175688+ChiyannQ@users.noreply.github.com>
2018-11-20 23:01:20 +0100

    Update MIA_2018_QIU_14.m

--- MIA_2018_QIU_14.m
+++ MIA_2018_QIU_14.m
@@ -1,68 +1,87 @@
 
 
-
-
 % read
 I = imread('cameraman.tif');
 
 % display the linear kernels of the following filter
 
-h_disk = fspecial('disk',10);% 21-by-21 matrix
-h_laplacian = fspecial('laplacian',0.3); %3-by-3
-h_sobel = fspecial('sobel');%3-by-3
+h_disk = fspecial('disk',10)% 21-by-21 matrix
+h_laplacian = fspecial('laplacian',0.3) %3-by-3
+h_sobel = fspecial('sobel')%3-by-3
 
+figure(1)
+subplot(2,2,1)
+stem3(h_disk)
+title('disk filter')
+subplot(2,2,2)
+stem3(h_laplacian)
+title('laplacian filter')
+subplot(2,2,3)
+stem3(h_sobel)
+title('sobel filter')
 
 % display the frequency responses of the 3 filter
-
+fxshift = -128/2:128/2-1;
+fyshift = -128/2:128/2-1;  
     % fourier transform
-H_disk = fft2(h_disk,128,128);
-H_laplacian = fft2(h_laplacian,128,128);
-H_sobel = fft2(h_sobel,128,128);
+H_disk = abs(fft2(h_disk,128,128))./128;
+H_laplacian = abs(fft2(h_laplacian,128,128))./128;
+H_sobel = abs(fft2(h_sobel,128,128))./128;
+    % shift
+Hs_disk = fftshift(H_disk);
+Hs_laplacian = fftshift(H_laplacian);
+Hs_sobel = fftshift(H_sobel);
 
-[H_d,w_d] = freqz2(h_disk,10);
-[H_la,w_la] = freqz2(h_laplacian,10);
-[H_s,w_s] = freqz2(h_sobel,10);
+figure(2)
+subplot(3,1,1)
+surf(fxshift,fyshift,Hs_disk)
+title('frequency response ofdisk filter')
+subplot(3,1,2)
+surf(fxshift,fyshift,Hs_laplacian)
+title('frequency response of laplacian filter')
+subplot(3,1,3)
+surf(fxshift,fyshift,Hs_sobel)
+title('frequency response of sobel filter')
 
+% Compare the filter
+figure(3)
+subplot(2,2,1)
+imshow(h_disk)
+title('disk filter in the space domain')
+subplot(2,2,2)
+surf(fxshift,fyshift,Hs_disk)
+title('frequency response ofdisk filter')
+subplot(2,2,3)
+imshow(I)
+title('originalIma')
+subplot(2,2,4)
+imshow(imfilter(I,h_disk))
+title('filter image by disk filter')
 
-% display
+figure(4)
+subplot(2,2,1)
+imshow(h_laplacian)
+title('laplacian filter in the space domain')
+subplot(2,2,2)
+surf(fxshift,fyshift,Hs_laplacian)
+title('laplacian filter in the frequency domain')
+subplot(2,2,3)
+imshow(I)
+title('originalIma')
+subplot(2,2,4)
+imshow(imfilter(I,h_laplacian))
+title('filter image by laplacian filter')
 
-%figure(1)
-
-%subplot(1,4,1)
-%imshow(h_disk)
-%title()
-
-%subplot(1,4,2)
-%imshow(H_disk)
-%title()
-
-%subplot(1,4,3)
-%imshow(I)
-%title('originalIma')
-
-%subplot(1,4,4)
-%imshow(I_disk)
-%title()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+figure(5)
+subplot(2,2,1)
+imshow(h_sobel)
+title('sobel filter in the space domain')
+subplot(2,2,2)
+surf(fxshift,fyshift,Hs_sobel)
+title('sobel filter in the frequency domain')
+subplot(2,2,3)
+imshow(I)
+title('originalIma')
+subplot(2,2,4)
+imshow(imfilter(I,h_sobel))
+title('filter image by sobel filter')
